74cbca9ebb1b33cd38c90d003d2ff5f7489ce925
Serhii Kochniev <sergeykochnev4149@gmail.com>
2022-12-29 19:48:39 +0200

    Added asynchronous sending and receiving of API requests using Retrofit2. The obtained results are displayed on the screen

---
+++ app/src/main/java/com/example/mornhousetesttask/FactAboutNumber.java
@@ -0,0 +1,31 @@
+package com.example.mornhousetesttask;
+
+public class FactAboutNumber {
+
+
+    private String text;
+    private String number;
+
+
+    public FactAboutNumber(String text, String number) {
+        this.text = text;
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/mornhousetesttask/MainActivity.java
+++ app/src/main/java/com/example/mornhousetesttask/MainActivity.java
@@ -2,13 +2,78 @@ package com.example.mornhousetesttask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 
 public class MainActivity extends AppCompatActivity {
+    static String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getFact(10);
     }
+
+
+
+    private void getFact(int number) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://numbersapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<FactAboutNumber> call = retrofitAPI.getFact(number);
+        call.enqueue(new Callback<FactAboutNumber>() {
+            @Override
+            public void onResponse(Call<FactAboutNumber> call, Response<FactAboutNumber> response) {
+                if (response.isSuccessful()) {
+                    FactAboutNumber modal = response.body();
+                    TextView fact = (TextView) findViewById(R.id.text);
+                    fact.setTextColor(Color.WHITE);
+                    fact.setText(modal.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FactAboutNumber> call, Throwable t) {
+                // displaying an error message in toast
+                Toast.makeText(MainActivity.this, "Fail to get the data..", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+//    public String getFact(int number){
+//
+//        return "";
+//    }
+
+//    private String getFactAboutRandomNumber (){
+//
+//        return "";
+//    }
+
+
+
+    public interface RetrofitAPI {
+        @GET("{number}?json")
+        Call<FactAboutNumber> getFact(@Path("number") int number);
+    }
+
+
+
+
 }
+
